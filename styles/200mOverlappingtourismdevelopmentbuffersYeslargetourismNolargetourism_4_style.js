3bc44214dc89b0d9ab28e693bcbd3a2e6be20b1c
NieminenLauri <lauri.k.nieminen@utu.fi>
2025-05-30 10:26:36 +0300

    Changing layer 2 styles
    changes in the style code of zonation buffer layers

--- styles/200mOverlappingtourismdevelopmentbuffersYeslargetourismNolargetourism_4_style.js
+++ styles/200mOverlappingtourismdevelopmentbuffersYeslargetourismNolargetourism_4_style.js
@@ -1,36 +1,27 @@
-var size = 0;
-var placement = 'point';
-
-var style_200mOverlappingtourismdevelopmentbuffersYeslargetourismNolargetourism_4 = function(feature, resolution){
-    var context = {
-        feature: feature,
-        variables: {}
-    };
-    
+var style_200mOverlappingtourismdevelopmentbuffersYeslargetourismNolargetourism_4 = function(feature, resolution) {
     var labelText = ""; 
-    var value = feature.get("");
-    var labelFont = "10px, sans-serif";
+    var labelFont = "10px sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
-    var textAlign = "left";
-    var offsetX = 0;
-    var offsetY = 0;
     var placement = 'point';
-    if ("" !== null) {
-        labelText = String("");
-    }
-    var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(228,26,28,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    }),new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(228,26,28,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.748}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
 
-    return style;
+    return [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'rgba(228,26,28,1.0)', 
+                width: 1.748,
+                lineCap: 'square',
+                lineJoin: 'bevel',
+                lineDash: null
+            }),
+            fill: new ol.style.Fill({
+                color: 'rgba(228,26,28,0.3)' // hieman läpinäkyvä punainen täyttö
+            }),
+            text: createTextStyle(
+                feature, resolution, labelText, labelFont,
+                labelFill, placement, bufferColor, bufferWidth
+            )
+        })
+    ];
 };
